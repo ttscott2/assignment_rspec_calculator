6e458abea8112c55e562f8845a5a637ae8c34279
Tim Scott <timothytscott3@gmail.com>
2016-11-16 21:28:33 +0000

    finish #add test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,12 @@
 # Your code here
+require_relative 'spec_helper.rb'
+require 'calculator.rb'
+
+
+describe Calculator do
+  describe '#add' do
+    it 'returns the sum of its arguments' do
+      expect(Calculator.new.add(1, 2)).to eq(3)
+    end
+  end
+end
